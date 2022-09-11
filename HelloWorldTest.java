4bb3217feef5d9222acf4d5fdb9fcdcd409c9154
itsmohanchow <52829071+itsmohanchow@users.noreply.github.com>
2022-09-11 11:24:53 +0530

    Add files via upload

---
+++ HelloWorldTest.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.helloworld;
+public class HelloWorldTest 
+{   
+    public HelloWorldTest() 
+    {
+    }
+
+  
+    @org.junit.jupiter.api.Test
+    public void testMain() 
+    {
+        System.out.println("Hello World");
+    }
+    
+}
